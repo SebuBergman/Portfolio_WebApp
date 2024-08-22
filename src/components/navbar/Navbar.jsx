f5102d7157600502c598cd8ec4305bc2965c3ea9
SebuBergman <basti.bergman@gmail.com>
2024-08-23 00:06:39 +0300

    added social icons back to navbar. Still need to make a secondary navbar or make it transparent so it can be sticky

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -82,6 +82,10 @@ function Navbar() {
               ))}
             </Box>
           </div>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/sebastian-bergman-01061679/" target="_blank"><img src="/LinkedIn.webp" alt="LinkedIn logo" /></a>
+            <a href="https://github.com/SebuBergman" target="_blank"><img src="/github-mark-white.png" alt="Github logo" /></a>
+          </div>
         </div>
       </div>
       <nav>
