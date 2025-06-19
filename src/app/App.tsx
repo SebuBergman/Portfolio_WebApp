9eb90129c9405841941f5e08b650b23823c28f36
Sebastian Bergman <basti.bergman@proton.me>
2025-06-19 18:18:37 +0300

    feat: added redux persistGate, nextuiprovider and localization to my portfolio page

---
+++ src/app/App.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyles } from "@app/config/styles/Global";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { theme } from "@config/styles";
+import { useAuthStateSubscription } from "@services/firebase";
+
+import { AppRouter } from "./config/routes";
+import "./app.scss";
+import { PersistGate } from "redux-persist/integration/react";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import Loader from "@features/ui/Loader";
+import { persistor } from "./store/index";
+import { ThemeProvider } from "@mui/material";
+
+function App() {
+  useAuthStateSubscription();
+
+  return (
+    <PersistGate loading={<Loader />} persistor={persistor}>
+      <BrowserRouter>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <ThemeProvider theme={theme}>
+            <NextUIProvider>
+              <GlobalStyles />
+              <AppRouter />
+            </NextUIProvider>
+          </ThemeProvider>
+        </LocalizationProvider>
+      </BrowserRouter>
+    </PersistGate>
+  );
+}
+
+export default App;
