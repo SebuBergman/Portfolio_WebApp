198830bd498dbd753b9e58ede6f6b9096692a406
Sebastian Bergman <basti.bergman@proton.me>
2025-06-19 18:30:01 +0300

    feat: created a booklist page for future dev and a dashboard page for my movieLibrary application part. Also date formatting.

---
+++ src/app/services/date/formatDate.ts
@@ -0,0 +1,12 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
+
+export function formatDate(date: Date | null, format: string) {
+  return dayjs(date).format(format);
+}
+
+export function timeAgo(timestamp: string): string {
+  return dayjs(timestamp).fromNow();
+}
---
+++ src/pages/not-found.tsx
@@ -0,0 +1,33 @@
+import Footer from "@features/footer";
+import Header from "@features/header";
+import { Box, Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import pageNotFound from "@assets/images/page_not_found.webp";
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          p: { xs: "0px 10px 25px 0px", md: "0px 10px" },
+        }}
+      >
+        <Box
+          component="img"
+          src={pageNotFound}
+          alt="Carousel 1"
+          sx={{
+            height: { xs: "100%" },
+            width: { xs: "100%", md: "680px" },
+            borderRadius: "30px",
+          }}
+        />
+        <p>
+          <Link to="/">Click here to go back to the landing page</Link>
+        </p>
+      </Stack>
+    </div>
+  );
+}
