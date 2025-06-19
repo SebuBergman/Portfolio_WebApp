d0b6284ec2ea9e7f029a066df930922469367624
Sebastian Bergman <basti.bergman@proton.me>
2025-06-19 18:17:26 +0300

    fix: changed from javascript to typescript and from yarn to npm

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,15 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2021: true },
+  env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.3.1" } },
+  parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "react/prop-types": "off",
-    "no-unused-vars": "warn",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@config": path.resolve(__dirname, "./src/app/config"),
+      "@assets": path.resolve(__dirname, "./src/app/assets"),
+      "@store": path.resolve(__dirname, "./src/app/store"),
+      "@services": path.resolve(__dirname, "./src/app/services"),
+      "@hooks": path.resolve(__dirname, "./src/app/hooks"),
+      "@movies": path.resolve(__dirname, "./src/features/movieLibrary/movies"),
+      "@tvShows": path.resolve(
+        __dirname,
+        "./src/features/movieLibrary/tvShows"
+      ),
+    },
+  },
+  plugins: [react()],
+});
