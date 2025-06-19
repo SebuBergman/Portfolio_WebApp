8a17e1f57846be0b9bdd2ad82f2223f2d4bc2438
Sebastian Bergman <basti.bergman@proton.me>
2025-06-19 18:25:00 +0300

    feat: added firebase service for db

---
+++ src/app/services/firebase/index.ts
@@ -0,0 +1,4 @@
+export { auth } from "./firebase";
+export { firestore } from "./firebase";
+export { mapAuthCodeToMessage } from "./helpers/mapAuthCodeToMessage";
+export { useAuthStateSubscription } from "./hooks/useAuthStateSubscription";
