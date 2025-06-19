d02585af1b0e8517c34efa0b9df227a201c09608
Sebastian Bergman <basti.bergman@proton.me>
2025-06-19 18:25:35 +0300

    feat: added routing for my new part of my application

---
+++ src/app/config/routes/AppRoutes.ts
@@ -0,0 +1,10 @@
+export const AppRoutes = {
+  home: "/",
+  login: "/login",
+  signUp: "/sign-up",
+  portfolio: "/portfolio",
+  movielibrary: "/leffakirjasto",
+  tvShowList: "/leffakirjasto/tv-shows",
+  movieList: "/leffakirjasto/movies",
+  bookList: "/leffakirjasto/books",
+};
---
+++ src/app/config/routes/components/AppRouter.tsx
@@ -0,0 +1,46 @@
+import { Route, Routes } from "react-router-dom";
+
+import NotFoundPage from "@pages/not-found";
+import AuthLayout from "@features/ui/layout/AuthLayout";
+//import SignUpPage from "@pages/register";
+import LoginPage from "@pages/login";
+import Portfolio from "@pages/portfolio";
+
+import { AppRoutes } from "../AppRoutes";
+import AccountLayout from "@features/ui/layout/accountLayout/AccountLayout";
+import ProtectedRoute from "./ProtectedRoute";
+import MovieList from "@pages/account/movieList";
+import TVShowList from "@pages/account/tvShowList";
+import DashboardPage from "@pages/account/dashboard";
+import BookList from "@pages/account/bookList";
+
+export default function AppRouter() {
+  return (
+    <Routes>
+      {/* BergmanWebWorks */}
+      {/*<Route path={AppRoutes.home} element={<FrontPage />} />*/}
+      {/* Portfolio */}
+      <Route path={AppRoutes.home} element={<Portfolio />} />
+
+      <Route element={<AuthLayout />}>
+        {/*<Route path={AppRoutes.signUp} element={<SignUpPage />} />*/}
+        <Route path={AppRoutes.login} element={<LoginPage />} />
+      </Route>
+      {/* User account Pages */}
+      <Route
+        element={
+          <ProtectedRoute>
+            <AccountLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route path={AppRoutes.movielibrary} element={<DashboardPage />} />
+        <Route path={AppRoutes.movieList} element={<MovieList />} />
+        <Route path={AppRoutes.tvShowList} element={<TVShowList />} />
+        <Route path={AppRoutes.bookList} element={<BookList />} />
+      </Route>
+      {/* Not Found Page */}
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
