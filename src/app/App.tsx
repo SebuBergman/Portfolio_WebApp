158496d1ec603a07795cdbdfcf330a295ad99cf6
SebuBergman <basti.bergman@proton.me>
2025-09-16 21:07:51 +0300

    fix: probably fixed google analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
 import { GlobalStyles } from "@app/config/styles/Global";
 
-import { NextUIProvider } from "@nextui-org/react";
+import { HeroUIProvider } from "@heroui/react";
 import { theme } from "@config/styles";
 import { useAuthStateSubscription } from "@services/firebase";
 
@@ -13,34 +12,20 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Loader from "@features/ui/Loader";
 import { persistor } from "./store/index";
 import { ThemeProvider } from "@mui/material";
-import ReactGA from "react-ga";
-import { useEffect } from "react";
-
-const TRACKING_ID = import.meta.env.VITE_TRACKING_ID;
+import { useAnalytics } from "./hooks/useAnalytics";
 
 function App() {
-  useEffect(() => {
-    if (TRACKING_ID) {
-      ReactGA.initialize(TRACKING_ID);
-      ReactGA.pageview(window.location.pathname + window.location.search);
-    } else {
-      console.warn("Google Analytics tracking ID not found");
-    }
-  }, []);
+  useAnalytics();
   useAuthStateSubscription();
 
   return (
     <PersistGate loading={<Loader />} persistor={persistor}>
-      <BrowserRouter>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <ThemeProvider theme={theme}>
-            <NextUIProvider>
-              <GlobalStyles />
-              <AppRouter />
-            </NextUIProvider>
-          </ThemeProvider>
-        </LocalizationProvider>
-      </BrowserRouter>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <AppRouter />
+        </ThemeProvider>
+      </LocalizationProvider>
     </PersistGate>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
